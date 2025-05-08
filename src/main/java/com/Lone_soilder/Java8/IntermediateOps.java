97011b31fed879af937a2677ebdb9fc5d00cfc48
Biswajit Sahoo <160817942+Lone-soilder@users.noreply.github.com>
2025-05-08 16:10:34 +0530

    Update IntermediateOps.java

--- src/main/java/com/Lone_soilder/Java8/IntermediateOps.java
+++ src/main/java/com/Lone_soilder/Java8/IntermediateOps.java
@@ -36,6 +36,45 @@ public class IntermediateOps {
 
         //6. skip
         System.out.println(Stream.iterate(1, x->x+2 ).limit(20).skip(10).count());
+        // 7.peek
+         //performs an action on each element as it is consumed. same as for each loop
+         Stream.iterate(1, x-> x+1).limit(20).skip(5).peek(System.out::println).count();
+        
+         // 8. flatMap
+         // handle streams of collections , lists , or arrays where each element is itself a collection means list of list
+         // flatten nested structures (e.g. lists within lists) so that they can be processed as a single sequence of elements
+         // transform and flatten elements at the same time
+        
+         List<List<String>> listOfLists = Arrays.asList(
+         Arrays.asList("apple", "banana"),
+         Arrays.asList("carrot" , "kiwi"),
+         Arrays.asList("pear", "grape")
+         );
+        
+         System.out.println(listOfLists.get(1).get(1)); // kiwi
+        
+         // make every element into uppercase and should be in flat
+         List<String> upperCaseFruits = listOfLists.stream().flatMap(x -> x.stream()).map(String::toUpperCase).toList();
+         System.out.println(upperCaseFruits); //[APPLE, BANANA, CARROT, KIWI, PEAR, GRAPE]
+        
+         //example - turn each word into uppercase from the sentence
+         List<String> sentences = Arrays.asList(
+         "Hello world",
+         "java stream is tough",
+         "flatmap is nice"
+         );
+        
+         List<String> upperCaseWords =
+         sentences
+         .stream()
+         .flatMap(sentence -> Arrays.stream(sentence.split(" "))
+         .map(String::toUpperCase))
+         .toList();
+         System.out.println(upperCaseWords); //[HELLO, WORLD, JAVA, STREAM, IS, TOUGH, FLATMAP, IS, NICE]
+        
+        
+
+Maximize 
 
     }
 }
